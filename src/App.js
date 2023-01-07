de0a145373947778f42c867eb8c52b4d2166a295
aswilson <aswilson87@gmail.com>
2023-01-07 15:15:33 -0500

    Added amplify yml

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is our app to test Amplify service!
         </p>
         <a
           className="App-link"
