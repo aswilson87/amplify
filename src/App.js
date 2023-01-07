150517f223a5099ad6fc7fd908dc22f105211660
aswilson <aswilson87@gmail.com>
2023-01-07 15:38:12 -0500

    testing auto building

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import {useEffect, useState} from "react";
 
 function App() {
+  const [poof, setPoof] = useState(null)
+
+  useEffect(() => {
+    axios.get('https://random-data-api.com/api/v2/users').then(r => r.data).then(data => setPoof(data.id))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           This is our app to test Amplify service!
+          { poof }
         </p>
         <a
           className="App-link"
